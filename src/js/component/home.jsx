4b61462904fc015336ed7ee2aab86d24afe65de8
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-03-14 22:09:42 +0000

    version finalizada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,114 +6,180 @@ import React, { useState, useEffect } from "react";
 //create your first component
 const Home = () => {
 
-	const [number, setNumber] = useState(10)
-	const [number2, setNumber2] = useState(5)
-	const [number3, setNumber3] = useState(8)
-	
+	const [numberRed, setNumberRed] = useState('')
+	const [numberYellow, setNumberYellow] = useState('')
+	const [numberGreen, setNumberGreen] = useState('')
+
+
 
 	/* VARIABLE DE HOOK */
-	const [color, setColor] = useState(false)
-	const [color2, setColor2] = useState(false)
-	const [color3, setColor3] = useState(false)
- 
-	/* COLORS */
+	const [colorRed, setColorRed] = useState(false)
+	const [colorYellow, setColorYellow] = useState(false)
+	const [colorGreen, setColorGreen] = useState(false)
+	const [colorPurple, setColorPurple] = useState(false)
+
+	/* SELECT COLORS */
 	const colorSelect = () => {
-		setColor(!color)
+		setColorRed(!colorRed)
+		setNumberRed(10)
 	}
 
-	const colorSelect2 = () => {
-		SetColor2(!color2)
+	const restart = () => {
+		setNumberRed('')
+		setNumberYellow('')
+		setNumberGreen('')
+		setColorRed(false)
+		setColorYellow(false)
+		setColorGreen(false)
 	}
 
-	const colorSelect3 = () => {
-		setColor3(!color3)
+
+	/* CONDICIONES DE LOS ESTADOS */
+
+	/* COLOR RED */
+
+	if (numberRed == -1) {
+		setColorRed(false)
+		setColorYellow(true)
+		setNumberYellow(3)
+		setNumberRed('')
 	}
 
+
+	/* COLOR YELLOW */
 	
-
-	if(number == -1){
-		setColor(false)
-		setNumber(10)
-		setColor2(true)
+	if (numberYellow == -1) {
+		setColorYellow(false)
+		setColorGreen(true)
+		setNumberGreen(12)
+		setNumberYellow('')
 	}
 
-	if(number2 == -1){
-		setColor2(false)
-		setNumber2(5)
-		setColor3(true)
-	}
 
-	if(number3 == -1){
-		setColor3(false)
-		setNumber3(8)
-		setColor(true)
+	/* COLOR GREEN */
+	
+	if (numberGreen == -1) {
+		setColorGreen(false)
+		setColorRed(true)
+		setNumberRed(10)
+		setNumberGreen('')
+		
 	}
 
 	/* USE EFFECT */
 	useEffect(() => {
 		let interval = 0
-		if (color || color2 || color3) {
-		  interval = setInterval(() => {
-			color ? setNumber(number - 1) : setNumber(10)
-			color2 ? setNumber2(number2 - 1) : setNumber2(5)
-			color3 ? setNumber3(number3 - 1) : setNumber3(8)
-		  }, 1000)
-		} else if (!color) {
-		  clearInterval(interval)
+		if (colorRed || colorYellow || colorGreen) {
+			interval = setInterval(() => {
+				if(colorRed) {setNumberRed(numberRed - 1)} 
+				if(colorYellow) {setNumberYellow(numberYellow - 1)} 
+				if(colorGreen) {setNumberGreen(numberGreen - 1)} 
+			}, 1000)
+		} else {
+			clearInterval(interval)
 		}
 
-		
-	
+
+
 		return () => clearInterval(interval)
-	  }, [color, color2, color3, number, number2, number3])
+	}, [colorRed, colorYellow, colorGreen, numberRed, numberYellow, numberGreen])
 
 	return (
-		<div>
-			<h1 className="text-center mt-5">SEMAFORO</h1>
-			<button onClick={colorSelect}>{color ? 'Pausar' : 'Iniciar'}{number}</button>
+		<div className="container">
+			<div className="row">
+				<div className="col mt-3">
+					<div className="d-flex justify-content-center">
+						<h1 className="border rounded text-center mt-2 p-2" style={{ maxWidth: '350px', backgroundColor: '#eaeaea' }}>SEMAFORO</h1>
+					</div>
+					<div className="mt-5 p-3 text-center border rounded d-flex flex-column" style={{backgroundColor: '#eaeaea'}}>
+						{/* BOTON DE INICIAR */}
+						<button className={`mb-2 btn btn-${colorRed ? 'danger' : 'success'} text-center`} 
+						onClick={colorSelect} 
+						disabled={colorYellow || colorGreen ? true : false} >
+							{colorRed ? 'Pausar en este color' : 'Iniciar'}
+						</button>
 
-			<div className="d-flex justify-content-center">
-			<div className="bg-dark rounded p-5 d-flex flex-column justify-content-center" style={{maxWidth: '300px'}}>
-				<div 
-				className={`border display-1 text-center mb-3 rounded bg-${color ? 'danger' : 'light'}`} 
-				href="#" 
-				/* onMouseOver={colorSelect} 
-				onMouseLeave={colorSelect}  */
-				style={{width: '200px', 
-				height: '200px', 
-				borderRadius: '50%', 
-				boxShadow: `${color ? '0 0 50px rgb(255, 34, 0)' : ''}`
-				}}>{number}</div>
+						{/* BOTON DE REINICIAR */}
+						<button className={`mb-2 btn btn-outline-success text-center`} onClick={restart}>Reiniciar</button>
+
+						{/* BOTON DE ADD COLOR PURPLE */}
+						<button className={`mb-2 btn text-center`} onClick={() => setColorPurple(!colorPurple)}
+							style={{ backgroundColor: `${colorPurple ? '#a700a7' : '#f99ef9'}` }}
+						>Add Color Purple</button>
+					</div>
+				</div>
+
+				<div className="col mt-3">
+					<div className="d-flex justify-content-center">
+						<div className="bg-dark rounded p-5 d-flex flex-column justify-content-center" style={{ maxWidth: '300px', minHeight: '700px' }}>
+							<div
+								className='display-1 text-center mb-3 bg-danger d-flex align-items-center justify-content-center'
+								/* onMouseOver={colorSelect} 
+								onMouseLeave={colorSelect}  */
+								style={{
+									width: '200px',
+									height: '200px',
+									borderRadius: '50%',
+									boxShadow: `${colorRed ? '0px 0px 100px 50px rgb(255, 34, 0)' : ''}`
+								}}><div><p style={{ color: '#fc6666' }}>{numberRed}</p></div></div>
 
 
 
-				<div 
-				className={`border display-1 text-center mb-3 rounded bg-${color2 ? 'warning' : 'light'}`} 
-				href="#" 
-				/* onMouseOver={colorSelect2} 
-				onMouseLeave={colorSelect2}  */
-				style={{width: '200px', 
-				height: '200px', 
-				borderRadius: '50%',
-				boxShadow: `${color2 ? '0 0 50px yellow' : ''}`
-				}}>{number2}</div>
+							<div
+								className='display-1 text-center mb-3 bg-warning d-flex align-items-center justify-content-center'
+								/* onMouseOver={colorSelect2} 
+								onMouseLeave={colorSelect2}  */
+								style={{
+									width: '200px',
+									height: '200px',
+									borderRadius: '50%',
+									boxShadow: `${colorYellow ? '0px 0px 100px 50px yellow' : ''}`
+								}}><div><p style={{ color: '#f6f698' }}>{numberYellow}</p></div></div>
 
 
 
-				<div 
-				className={`border display-1 text-center rounded bg-${color3 ? 'success' : 'light'}`} 
-				href="#" 
-				/* onMouseOver={colorSelect3} 
-				onMouseLeave={colorSelect3}  */
-				style={{width: '200px', 
-				height: '200px', 
-				borderRadius: '50%',
-				boxShadow: `${color3 ? '0 0 50px rgb(0, 255, 38)' : ''}`
-				}}>{number3}</div>
-			</div>
+							<div
+								className='display-1 text-center bg-success
+						d-flex align-items-center justify-content-center'
+								/* onMouseOver={colorSelect3} 
+								onMouseLeave={colorSelect3}  */
+								style={{
+									width: '200px',
+									height: '200px',
+									borderRadius: '50%',
+									boxShadow: `${colorGreen ? '0px 0px 100px 50px rgb(0, 255, 38)' : ''}`
+								}}>
+								<div><p style={{ color: '#31f548' }}>{numberGreen}</p></div>
+							</div>
+
+							{/* COLOR PURPLE */}
+							{colorPurple && (
+								<div
+									className='display-1 text-center
+					d-flex align-items-center justify-content-center mt-3'
+									style={{
+										width: '200px',
+										height: '200px',
+										backgroundColor: '#a700a7',
+										borderRadius: '50%',
+										boxShadow: `${colorPurple ? '0px 0px 100px 50px #a700a7' : ''}`
+									}}>
+
+								</div>
+							)}
+						</div>
+					</div>
+					{/* ESTO ES EL CUADRADO DE ABAJO */}
+					<div className="d-flex justify-content-center">
+						<div className="bg-dark rounded-bottom" style={{ width: '50px', height: '16vh' }}>
+							.
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
 };
 
 export default Home;
+
